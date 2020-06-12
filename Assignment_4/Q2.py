99c40038a43e5ce0d4b762b175383e8000d9ce11
Jugal Deepak Chauhan <jugaldc2000@gmail.com>
2020-06-12 13:49:44 +0530

    submission

---
+++ Assignment_4/Q2.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+img=cv2.imread('car.jpeg')
+
+p = False
+q = False
+
+def crop_func(event,x,y,flags,param):
+    global pts,p,q
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if p == False:
+            pts = [(x,y)]
+            p = True
+        elif q == False:
+            pts.append((x,y))
+            q = True
+        if len(pts) == 2:
+            crop = img[ty:by, tx:bx]
+            cv2.imshow('Frame',crop)
+            
+cv2.namedWindow('Frame')
+cv2.setMouseCallback('Frame',crop_func)
+cv2.imshow('Frame',img)
