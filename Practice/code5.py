35c26739dcdb8654a0db59738c55b518e11c2d64
Jugal Deepak Chauhan <jugaldc2000@gmail.com>
2020-06-19 08:16:40 +0530

    updation

--- Practice/code5.py
+++ Practice/code5.py
@@ -1,7 +1,17 @@
+#code to draw shaped on an image and their parameter properties
+
 import cv2
 
 img = cv2.imread('dog.jpg')
 
 cv2.line(img, (0,0), (1000,1000), (0,255,255), 4)
+cv2.circle(img, (200,200), 20, (255,0,0), -1)
+cv2.rectangle(img, (100,100), (200,200), (255,255,0), -1)
+cv2.putText(img, 'mozzarella', (100,100), cv2.FONT_HERSHEY_SIMPLEX, 5, (100,100,245), 2)
+
+img[300:700, 400:700] = [0,0,0]
+
+cv2.imwrite('imwrite.jpg', img)
+
 cv2.imshow('frame', img)
 cv2.waitKey(0)
